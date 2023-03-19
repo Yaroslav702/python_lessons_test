e0f3f61b3bf533d0d5b92e5c62857177e137bd81
Yaroslav <manypresents34@gmail.com>
2023-03-19 11:32:44 +0200

    modules import lesson

---
+++ lesson18/hello.py
@@ -0,0 +1,12 @@
+def say_hello(name):
+    print('Hello', name)
+
+
+def main():
+    print('Successfully started from origin')
+    say_hello('Superuser')
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ lesson18/main.py
@@ -0,0 +1,45 @@
+# import fibo as fibonacci
+from fibo import fib2 as fibonacci
+import math
+from hello import say_hello as greetings
+
+# users_pi = 3.14
+# r = 5
+
+# result = users_pi * r**2
+# print(result)
+
+
+# modules_pi = math.pi
+# modules_result = modules_pi * r**2
+# print(round(modules_result, 4))
+
+
+# print(math.isclose(result, modules_result, rel_tol=0.001))
+
+# def hypotenusa(a, b):
+#     c = a**2 + b**2
+#     return math.sqrt(c)
+
+
+# def hypotenusa2(a, b):
+#     c = a**2 + b**2
+#     return c**0.5
+
+
+# print(hypotenusa(3, 4))
+# print(hypotenusa2(3, 4))
+
+
+
+greetings('Ivan')
+
+
+
+
+
+
+
+
+
+# fibonacci(233)
