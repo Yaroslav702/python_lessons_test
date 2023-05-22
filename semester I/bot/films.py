1a86f539e648e8049cc05f30cb61ddaa30e8da57
Yaroslav <manypresents34@gmail.com>
2023-05-22 08:32:50 +0300

    lesson 5 semester II

--- semester I/bot/films.py
+++ semester I/bot/films.py
@@ -20,45 +20,4 @@ films = {
 
 }
 
-shops = {
-    "Clothes": ['H&M', 'Adidas', 'Nike'],
-    "Technic": [('Apple', 'https://apple.com'), 'Samsung', 'Citrus', 'Rozetka'] 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-# goods = {
-#     "Apple Iphone 12 64 gb": {
-#         "site_url": "https://planetakino.ua/movies/dungeons-and-dragons-honor-among-thieves/#today",
-#         "photo": "https://planetakino.ua/res/get-poster/00000000000000000000000000002196/D%26D-vend.jpg",
-#         "description": "Діючи у парі Еджин (Кріс Пайн) та Хольга (Мішель Родрігес) викрали одну унікальну річ. Тоді вони не розуміли, що це може відкрити шлях небаченому раніше злу. Аби не допустити лиха, вони мають повернути артефакт на місце. Доведеться збирати цілу команду. Лише об’єднавши зусилля, вони зможуть попередити апокаліпсис, який випадково допомогли розпочати.",
-#         "rating": "7.6/10"
-#     },
-#     "HCMN2788": {
-#         "site_url": "https://planetakino.ua/movies/the-popes-exorcist/#today",
-#         "photo": "https://planetakino.ua/res/get-poster/00000000000000000000000000003927/PE-vend.jpg",
-#         "description": "Габріель Аморт (Рассел Кроу) став католицьким священиком у 29 років. У 41 рік отримав доленосне призначення. Він став головним екзорцистом Ватикану. З поміж сеансів вигнання нечистої сили, які стали для нього рутиною, особливо запам’ятався один випадок. Тоді отцю Габріелю довелося зазирнути в очі самому дияволу.",
-#         "rating": "6.1/10"
-#     },
-
-# }
-
-
-# cart = dict()
-
-
-# cart[callback_query.data] = {
-#         "description": goods[callback_query.data]['description']
-#     }
-
-# cart.clear()
 
